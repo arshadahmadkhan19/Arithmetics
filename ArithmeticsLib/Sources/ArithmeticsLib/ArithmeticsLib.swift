94b3259e5e898e422fa5efeeacd50cbce04b35a8
arshadahmadkhan19 <arshadahmadkhan@hotmail.com>
2023-10-01 22:51:26 -0400

    first-commit

---
+++ ArithmeticsLib/Sources/ArithmeticsLib/ArithmeticsLib.swift
@@ -0,0 +1,15 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import Foundation
+
+public class ArithmeticLib {
+    public init() {}
+    
+    public func add(_ num1: Int, num2: Int) -> Int {
+        num1 + num2
+    }
+    
+    public func sub(_ num1: Int, num2: Int) -> Int {
+        num1 - num2
+    }
+}
